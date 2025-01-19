eef7d5235ceb8dfaed97d73365e6635a854def8d
SamWorrod <samworrod@gmail.com>
2025-01-19 17:33:54 -0500

    changes to make app closer to what I want

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import { grey } from '@mui/material/colors';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { LineChart } from '@mui/x-charts/LineChart';
+import data from './data.js'
+import Grid from '@mui/material/Grid2';
 
+
+const generateTiers = () => {
+  let tiers = []
+  data.forEach((week) => {
+    tiers.push(week.tier)
+  })
+  return tiers
+}
+
+const generateLabels = () => {
+  let weeks = []
+  data.forEach((week) => {
+    weeks.push('Week ' + week.week.toString())
+  })
+  return weeks
+}
 function App() {
+  
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Box sx={{width: '100vw'}}>
+          <AppBar position="static" >
+            <Toolbar sx={{backgroundColor: '#475458'}}>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Blocked And Reported Dashboard
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
       </header>
+      <div className='body'>
+        <Box sx={{height: '60vh', width: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', color: 'black', boxShadow: '1px 1px 1px grey', margin: '15vh 5vh 0vh 5vh', border: '2px solid grey' }}>
+          <div style={{ width: '80vh', display: 'flex', flexDirection: 'row', justifyContent: 'center',}}>
+            Weekly Tier
+          </div>
+          
+          <LineChart
+            width={800}
+            height={500}
+            series={[
+              { data: generateTiers(), label: 'Current Tier' },
+            ]}
+            yAxis={[{min: 1, max: 16, label: 'Tier'}]}
+            xAxis={[{ scaleType: 'point', data: generateLabels(), label: 'Week' }]}
+          />
+        </Box>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,370 @@
+const data = [
+  {
+    week: 1,
+    tier: 10,
+    games: [
+      {
+        opponent: 'Sext-Men',
+        teamScore: 11,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Sext-Men',
+        teamScore: 20,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Big Digs',
+        teamScore: 18,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Big Digs',
+        teamScore: 23,
+        opponentScore: 25
+      }
+    ]
+  },
+  {
+    week: 2,
+    tier: 11,
+    games: [
+      {
+        opponent: 'Saved by the Balls',
+        teamScore: 25,
+        opponentScore: 10
+      },
+      {
+        opponent: 'Saved by the Balls',
+        teamScore: 26,
+        opponentScore: 27
+      },
+      {
+        opponent: 'Daddy Issues',
+        teamScore: 25,
+        opponentScore: 18
+      },
+      {
+        opponent: 'Daddy Issues',
+        teamScore: 15,
+        opponentScore: 25
+      }
+    ]
+  },
+  {
+    week: 3,
+    tier: 11,
+    games: [
+      {
+        opponent: 'Hungry Hungry Tippos',
+        teamScore: 25,
+        opponentScore: 16
+      },
+      {
+        opponent: 'Hungry Hungry Tippos',
+        teamScore: 25,
+        opponentScore: 10
+      },
+      {
+        opponent: 'Digma Balls',
+        teamScore: 25,
+        opponentScore: 13
+      },
+      {
+        opponent: 'Digma Balls',
+        teamScore: 25,
+        opponentScore: 22
+      }
+    ]
+  },
+  {
+    week: 4,
+    tier: 10,
+    games: [
+      {
+        opponent: 'Daddy Issues',
+        teamScore: 20,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Daddy Issues',
+        teamScore: 25,
+        opponentScore: 19
+      },
+      {
+        opponent: 'Late to the Game',
+        teamScore: 25,
+        opponentScore: 19
+      },
+      {
+        opponent: 'Late to the Game',
+        teamScore: 25,
+        opponentScore: 19
+      }
+    ]
+  },
+  {
+    week: 5,
+    tier: 9,
+    games: [
+      {
+        opponent: 'Big Digs',
+        teamScore: 13,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Big Digs',
+        teamScore: 17,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Set on my Face',
+        teamScore: 15,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Set on my Face',
+        teamScore: 13,
+        opponentScore: 25
+      }
+    ]
+  },
+  {
+    week: 6,
+    tier: 9,
+    games: [
+      {
+        opponent: 'Big Digs',
+        teamScore: 25,
+        opponentScore: 17
+      },
+      {
+        opponent: 'Big Digs',
+        teamScore: 25,
+        opponentScore: 20
+      },
+      {
+        opponent: 'Late to the Game',
+        teamScore: 16,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Late to the Game',
+        teamScore: 25,
+        opponentScore: 22
+      }
+    ]
+  },
+  {
+    week: 7,
+    tier: 8,
+    games: [
+      {
+        opponent: 'Block Destroyers',
+        teamScore: 25,
+        opponentScore: 19
+      },
+      {
+        opponent: 'Block Destroyers',
+        teamScore: 23,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Go Sportsball!',
+        teamScore: 20,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Go Sportsball!',
+        teamScore: 25,
+        opponentScore: 21
+      }
+    ]
+  },
+  {
+    week: 8,
+    tier: 8,
+    games: [
+      {
+        opponent: 'You Wont Break My Hole',
+        teamScore: 26,
+        opponentScore: 24
+      },
+      {
+        opponent: 'You Wont Break My Hole',
+        teamScore: 25,
+        opponentScore: 19
+      },
+      {
+        opponent: 'Set on my Face',
+        teamScore: 25,
+        opponentScore: 16
+      },
+      {
+        opponent: 'Set on my Face',
+        teamScore: 20,
+        opponentScore: 25
+      }
+    ]
+  },
+  {
+    week: 9,
+    tier: 7,
+    games: [
+      {
+        opponent: 'My Dumps',
+        teamScore: 15,
+        opponentScore: 25
+      },
+      {
+        opponent: 'My Dumps',
+        teamScore: 21,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Believe That',
+        teamScore: 25,
+        opponentScore: 23
+      },
+      {
+        opponent: 'Believe That',
+        teamScore: 23,
+        opponentScore: 25
+      }
+    ]
+  },
+  {
+    week: 10,
+    tier: 8,
+    games: [
+      {
+        opponent: 'You Wont Break My Hole',
+        teamScore: 14,
+        opponentScore: 25
+      },
+      {
+        opponent: 'You Wont Break My Hole',
+        teamScore: 27,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Set on my Face',
+        teamScore: 18,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Set on my Face',
+        teamScore: 18,
+        opponentScore: 25
+      }
+    ]
+  },
+  {
+    week: 11,
+    tier: 9,
+    games: [
+      {
+        opponent: 'Hungry Hungry Tippos',
+        teamScore: 25,
+        opponentScore: 12
+      },
+      {
+        opponent: 'Hungry Hungry Tippos',
+        teamScore: 25,
+        opponentScore: 18
+      },
+      {
+        opponent: 'Sext-Men',
+        teamScore: 27,
+        opponentScore: 26
+      },
+      {
+        opponent: 'Sext-Men',
+        teamScore: 25,
+        opponentScore: 19
+      }
+    ]
+  },
+  {
+    week: 12,
+    tier: 8,
+    games: [
+      {
+        opponent: 'A-Team',
+        teamScore: 25,
+        opponentScore: 22
+      },
+      {
+        opponent: 'A-Team',
+        teamScore: 27,
+        opponentScore: 26
+      },
+      {
+        opponent: 'Block Destroyers',
+        teamScore: 25,
+        opponentScore: 15
+      },
+      {
+        opponent: 'Block Destroyers',
+        teamScore: 25,
+        opponentScore: 6
+      }
+    ]
+  },
+  {
+    week: 13,
+    tier: 7,
+    games: [
+      {
+        opponent: 'My Dumps',
+        teamScore: 25,
+        opponentScore: 22
+      },
+      {
+        opponent: 'My Dumps',
+        teamScore: 18,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Go Sportsball!',
+        teamScore: 23,
+        opponentScore: 25
+      },
+      {
+        opponent: 'Go Sportsball!',
+        teamScore: 20,
+        opponentScore: 25
+      }
+    ]
+  },
+  {
+    week: 14,
+    tier: 8,
+    games: [
+      {
+        opponent: 'Sext-Men',
+        teamScore: 25,
+        opponentScore: 21
+      },
+      {
+        opponent: 'Sext-Men',
+        teamScore: 25,
+        opponentScore: 12
+      },
+      {
+        opponent: 'Block Destroyers',
+        teamScore: 25,
+        opponentScore: 18
+      },
+      {
+        opponent: 'Block Destroyers',
+        teamScore: 25,
+        opponentScore: 23
+      }
+    ]
+  },
+
+  
+]
+
+export default data
